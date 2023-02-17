ae130ee70a154d1db82120fafd497d676d1c52b6
Mathys GOUDAL <goudalmathys@gmail.com>
2023-02-17 17:30:18 +0100

    Page d'object Fonctionnelle

--- src/Containers/ObjectPage.js
+++ src/Containers/ObjectPage.js
@@ -1,71 +1,72 @@
-import React, { Component } from "react";
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import noImage from "../img/no-image.png";
 
-class ObjectPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        idObject: props.idObject,
-        dataObject: null
-    };
-  }
+const ObjectPage = () => {
+  const { idObject } = useParams();
+  const [dataObject, setDataObject] = useState(null);
 
-  componentDidMount() {
-    const fetchObject = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${this.state.idObject}`);
+        const response = await fetch(
+          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${idObject}`
+        );
         const data = await response.json();
-        this.setState({ dataObject: data });
+        setDataObject(data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchObject();
-  }
+    fetchData();
+  }, [idObject]);
 
-  render() {
-    const { dataObject } = this.state;
-    return (
-      <div className="p-8">
-        <h1 className="text-2xl font-bold">{dataObject.title}</h1>
-        <div className="mt-4">
-          <img
-            src={dataObject.primaryImage ? dataObject.primaryImage : noImage}
-            alt="Primary"
-            className="w-full h-auto"
-          />
+  return (
+    <div className="p-8">
+      {dataObject ? (
+        <div>
+          <h1 className="text-2xl font-bold">{dataObject.title}</h1>
+          <div className="mt-4">
+            <img
+              src={dataObject.primaryImage ? dataObject.primaryImage : noImage}
+              alt="Primary"
+              className="w-full"
+            />
+          </div>
+          <div className="mt-4">
+            <p className="text-lg font-bold">Description</p>
+            <p className="mt-2">{dataObject.objectName}</p>
+            <p className="mt-2">{dataObject.medium}</p>
+            <p className="mt-2">{dataObject.objectDate}</p>
+            <p className="mt-2">{dataObject.creditLine}</p>
+          </div>
+          <div className="mt-4">
+            <p className="text-lg font-bold">Dimensions</p>
+            <ul className="mt-2 list-disc list-inside">
+              {dataObject.measurements?.map((measurement) => (
+                <li key={measurement.elementName}>
+                  {measurement.elementName}:{" "}
+                  {measurement.elementMeasurements.Height} x{" "}
+                  {measurement.elementMeasurements.Width} x{" "}
+                  {measurement.elementMeasurements.Length}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-4">
+            <p className="text-lg font-bold">Location</p>
+            <p className="mt-2">
+              {dataObject.region}, {dataObject.subregion},{" "}
+              {dataObject.locale}
+            </p>
+          </div>
         </div>
-        <div className="mt-4">
-          <p className="text-lg font-bold">Description</p>
-          <p className="mt-2">{dataObject.objectName}</p>
-          <p className="mt-2">{dataObject.medium}</p>
-          <p className="mt-2">{dataObject.objectDate}</p>
-          <p className="mt-2">{dataObject.creditLine}</p>
-        </div>
-        <div className="mt-4">
-          <p className="text-lg font-bold">Dimensions</p>
-          <ul className="mt-2 list-disc list-inside">
-            {dataObject.measurements.map((measurement) => (
-              <li key={measurement.elementName}>
-                {measurement.elementName}:{" "}
-                {measurement.elementMeasurements.Height} x{" "}
-                {measurement.elementMeasurements.Width} x{" "}
-                {measurement.elementMeasurements.Length}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="mt-4">
-          <p className="text-lg font-bold">Location</p>
-          <p className="mt-2">
-            {dataObject.region}, {dataObject.subregion}, {dataObject.locale}
-          </p>
-        </div>
-      </div>
-    );
-  }
-}
+      ) : (
+        <p>Pas de données</p>
+      )}
+    </div>
+  );
+};
 
 export default ObjectPage;
